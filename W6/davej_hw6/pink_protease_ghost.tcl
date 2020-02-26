0d1774c30d8ea646871b8e15965de4ab2dd32c3f
Dave Juergens <davidcj@uw.edu>
2020-02-26 14:18:49 -0800

    Removed crappy tcl script and made a new one.

---
+++ W6/davej_hw6/pink_protease_ghost.tcl
@@ -0,0 +1,4 @@
+# VMD for MACOSXX86, version 1.9.4a38 (October 17, 2019)
+# Log file '/Users/DavidJuergens/Desktop/GradSchool/Classes/molsim/MOLSIM-2020/W6/davej_hw6/pink_protease_ghost.tcl', created by user DavidJuergens
+# VMD for MACOSXX86, version 1.9.4a38 (October 17, 2019)
+# end of log file.
