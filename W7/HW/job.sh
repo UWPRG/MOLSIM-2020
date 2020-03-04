373cefe27b35086e000e1d8e5e96a5cca356a707
Dave Juergens <davidcj@uw.edu>
2020-03-04 14:20:30 -0800

    I suck at coding

--- W7/HW/job.sh
+++ W7/HW/job.sh
@@ -16,12 +16,34 @@
 ## Memory per node
 #SBATCH --mem=22G
 
-%% The lines below indicate the path to GROMACS and PLUMED; this path will depend on simulation package in use %%
+# %% The lines below indicate the path to GROMACS and PLUMED; this path will depend on simulation package in use %%
  
-module load icc_15.0.3-impi_5.0.3
-source /gscratch/pfaendtner/cdf6gc/codes/GROMACS/activate_gmx2016.sh
+# module load icc_15.0.3-impi_5.0.3
+# source /gscratch/pfaendtner/cdf6gc/codes/GROMACS/activate_gmx2016.sh
 
-%% Command that calls on the GROMACS MD machine to run your simulation. Again, this may be different for different projects %%
-mpiexec.hydra -n 1 gmx_mpi mdrun -deffnm npt -ntomp 1 &> log.txt
+# walkers, Nodes, MPI ranks/walker, OpenMP threads/rank
+# |1 |1 |1 |1 |
+# |1 |1 |2 |1 |
+# |1 |1 |4 |1 |
+# |1 |1 |8 |1 |
+# |1 |1 |16 |1 |
+# |1 |1 |8 |2 |
+# |1 |1 |4 |4 |
+# |1 |1 |2 |8 |
+# |1 |1 |1 |16 |
+# |2 |1 |8 |1 |
+# |4 |1 |4 |1 |
+# |8 |1 |2 |1 |
+# |2 |2 |16 |1 |
+# |4 |2 |8 |1 |
+# |8 |2 |4 |1 |
 
 
+# %% Command that calls on the GROMACS MD machine to run your simulation. Again, this may be different for different projects %%
+# mpiexec.hydra -n 1 gmx_mpi mdrun -deffnm npt -ntomp 1 &> log.txt
+
+
+cat conditions.txt | while read line; do
+  
+  echo $line
+done
